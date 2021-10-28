7e83fd763284daed87fd426b52a9e02ea6e75f6d
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-10-28 22:41:47 +0200

    Navbar + logo update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,18 +8,31 @@ import {
   Menu,
   Typography,
 } from "@material-ui/core";
-import { CallMissedSharp, ShoppingCart } from "@material-ui/icons";
-
+import { ShoppingCart } from "@material-ui/icons";
+import logo from "../../assets/wrld.png";
 
 const Navbar = () => {
   return (
     <>
-      <AppBar position='fixed' className={classes.appBar} color='inherit'>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
-            <Typography variant='h6' className={classes.title} color='inherit'>
-                <img scr={} alt='Commerce' height='25px' className={CallMissedSharp.image}/>
-                Commerce
-            </Typography>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              scr={logo}
+              alt="Commerce"
+              height="25px"
+              className={classes.image}
+            />
+            Commerce
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </>
