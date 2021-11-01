66c23d8f701f328d945da836939e44dbdef6cb1e
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-11-01 20:10:47 +0100

    fetching products from lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
 
+import { commerce } from "./lib/commerce";
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
       <Navbar />
