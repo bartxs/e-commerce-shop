73daeb0526557c2c59453319883188bca245ad57
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-11-04 22:16:39 +0100

    Cart + Router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
+import Cart from "./components/Cart/Cart";
 import Navbar from "./components/Navbar/Navbar";
 import Products from "./components/Products/Products";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { commerce } from "./lib/commerce";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState();
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -32,10 +34,19 @@ const App = () => {
   console.log(cart);
 
   return (
-    <div>
-      <Navbar totalItems={cart.total_items} />
-      <Products products={products} onAddToCart={handleAddToCart} />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
