7dcc5d4a91c453225b0f0b5040eecaf1a1ebdd03
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-11-09 19:01:46 +0100

    address form update

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+
+import FormInput from "./CustomTextField";
 
 const AddressForm = () => {
-  return <div>addressform</div>;
+  const methods = useForm();
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping address
+      </Typography>
+      <FormProvider {...methods}>
+        <form onSubmit="l">
+          <Grid container spacing={3}>
+            <FormInput required name="firstName" label="First name" />
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
+  );
 };
 
 export default AddressForm;
