3b9ec2436424b58fbf8a698a2e52e35d80cf6f9f
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-11-10 18:09:12 +0100

    AddressForm update

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   InputLabel,
   Select,
@@ -12,6 +12,13 @@ import { useForm, FormProvider } from "react-hook-form";
 import FormInput from "./CustomTextField";
 
 const AddressForm = () => {
+  const [shippingCountries, setShippingCountries] = useState([])
+  const [shippingCountry, setShippingCountry] = useState('')
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([])
+  const [shippingSubdivision, setShippingSubdivision] = useState('')
+  const [shippingOptions, setShippingOptions] = useState([])
+  const [shippingOption, setShippingOption] = useState('')
+
   const methods = useForm();
 
   return (
@@ -28,6 +35,32 @@ const AddressForm = () => {
             <FormInput required name="email" label="E-mail" />
             <FormInput required name="city" label="City" />
             <FormInput required name="zip" label="ZIP / Postal code" />
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Me
+                </MenuItem>
+              </Select>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Subdivisions</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Me
+                </MenuItem>
+              </Select>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Options</InputLabel>
+              <Select value={} fullWidth onChange={}>
+                <MenuItem key={} value={}>
+                  Select Me
+                </MenuItem>
+              </Select>
+            </Grid>
           </Grid>
         </form>
       </FormProvider>
